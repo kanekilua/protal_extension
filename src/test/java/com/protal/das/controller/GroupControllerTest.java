34c7c6059e90c57f932d0f696f2a26e3f7d0a319
kane <kiluahaibin@Gmail.com>
2017-03-06 19:47:10 +0800

    protal_extension project

---
+++ src/test/java/com/protal/das/controller/GroupControllerTest.java
@@ -0,0 +1,21 @@
+package com.protal.das.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kane on 17-3-6.
+ */
+public class GroupControllerTest {
+    @Test
+    public void listName() throws Exception {
+
+    }
+
+    @Test
+    public void getCount() throws Exception {
+
+    }
+
+}
